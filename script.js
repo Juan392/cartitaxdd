281552222f1fb16ac221427dea1d36336a30ffb0
Juan392 <56325157+Juan392@users.noreply.github.com>
2024-11-16 10:47:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('play-music').addEventListener('click', () => {
+    const music = document.getElementById('birthday-music');
+    music.play();
+    alert('¡Que suene la fiesta! 🎉');
+  });
+  
